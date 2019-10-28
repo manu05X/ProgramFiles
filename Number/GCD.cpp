dd387e42a639526a97925f54c3e10219be79c0a9
MANISH KUMAR <36466808+manu05X@users.noreply.github.com>
2019-10-28 00:33:58 -0700

    GCD using Euclid Algo

---
+++ Number/GCD.cpp
@@ -0,0 +1,42 @@
+/*"Calculate and return GCD of two given numbers x and y. Numbers are within range of Integer.
+Input format :
+x and y (separated by space)
+Output format :
+GCD of x and y
+Sample Input 1:
+20 5
+Sample Output 1:
+5
+Sample Input 2:
+96 14
+Sample Output 2:
+2
+"*/
+
+
+
+#include<iostream>
+using namespace std;
+#include "solution.h"
+
+int main()
+{
+	int a,b;
+	cin>>a>>b;
+	cout<<gcd(a,b);
+	return 0;
+}
+int gcd(int a,int b)
+{
+    if(a < b)
+    {
+        return gcd(b,a);
+    }
+    if(b == 0)
+    {
+        return a;
+    }
+    return gcd(b, a%b);
+	//Write your code here
+}
+
