588af1093e5ac29b00f8098ce54b547359949473
MANISH KUMAR <36466808+manu05X@users.noreply.github.com>
2020-02-15 11:45:07 -0800

    Added via Upload

---
+++ InterviewBit/LinkedList/Merge_Two_Sorted_Lists.cpp
@@ -0,0 +1,78 @@
+/*
+Merge Two Sorted Lists
+Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists, and should also be sorted. For example, given following linked lists :
+  5 -> 8 -> 20 
+  4 -> 11 -> 15
+The merged list should be :
+4 -> 5 -> 8 -> 11 -> 15 -> 20
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ListNode* Solution::mergeTwoLists(ListNode* A, ListNode* B) {
+    if (A == NULL)
+		return B;
+	
+	else if (B == NULL)
+		return A;
+	
+	ListNode* result = NULL;
+   
+	// Pick either a or b, and recur
+	if (A->val <= B->val) 
+	{
+		result = A;
+		result->next = mergeTwoLists(A->next, B);
+	}
+	else 
+	{
+		result = B;
+		result->next = mergeTwoLists(A, B->next);
+	}
+
+	return result;
+}
+// ListNode* Solution::mergeTwoLists(ListNode* A, ListNode* B) {
+//     if(!A)
+//         return B;
+//     if(!B)
+//         return A;
+        
+//     ListNode *res = new ListNode(0); // Don't do ListNode* res = NULL; *resPtr or 
+//     ListNode *resPtr = res;          // filling the list gives segmentation fault.
+//     while(A && B)
+//     {
+//         if(A->val <= B->val)
+//         {
+//             res->next = A;
+//             A = A->next;
+//         }
+//         else
+//         {
+//             res->next = B;
+//             B = B->next;
+//         }
+//         res = res->next;
+//     }
+    
+//     while(A)
+//     {
+//         res->next = A;
+//         res = res->next;
+//         A = A->next;
+//     }
+//     while(B)
+//     {
+//         res->next = B;
+//         res = res->next;
+//         B = B->next;
+//     }
+//     return resPtr->next;
+// }
