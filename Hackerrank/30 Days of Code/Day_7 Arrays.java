acc178b59e59136e771886fe942bcbfc0b9fbc48
MANISH KUMAR <36466808+manu05X@users.noreply.github.com>
2020-03-25 10:02:44 -0700

    Added via Upload

---
+++ Hackerrank/30 Days of Code/Day_7 Arrays.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        
+        int n = scanner.nextInt();
+       int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) 
+        {
+            int idx = n - i - 1;
+            System.out.print(arr[idx] + " ");
+        }
+
+        scanner.close();
+    }
+}
