6ea2818d4f530f76b5aab64a45006bc21a027185
MANISH KUMAR <36466808+manu05X@users.noreply.github.com>
2020-10-13 20:04:11 -0700

    Added via Upload

--- InterviewBit/DP/Longest Palindromic Subsequence.cpp
+++ InterviewBit/DP/Longest Palindromic Subsequence.cpp
@@ -81,6 +81,56 @@ int Solution::solve(string A) {
 }
 
 
+//Method 2
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        int n = s.size();
+        vector<vector<int>> dp(n,vector<int>(n));
+        for(int i=0;i<n;++i)
+            dp[i][i] = 1;
+        for(int i=n-1;i>=0;--i){
+            for(int j=i+1;j<n;++j){
+                if(s[i] == s[j])
+                    dp[i][j] = dp[i+1][j-1]+2;
+                else
+                    dp[i][j] = max(dp[i+1][j],dp[i][j-1]);
+            }
+        }
+        return dp[0][n-1];
+    }
+};
+
+//Method 3
+class Solution {
+public:
+    int dp[1003][1003];
+    int longestPalindromeSubseq(string s) {
+        string t = s;
+		//reverse the given string
+        reverse(s.begin(),s.end());
+        int n = s.size();
+        // find longest common subsequence using dynamic programming
+        for(int i = 0;i<= n;i++)
+        {
+            for(int j = 0;j<= n;j++)
+            {
+                if(i == 0 || j == 0)
+                    dp[i][j] = 0;
+                else
+                {
+                    if(t[i -1] == s[j -1])
+                        dp[i][j] = 1 + dp[i-1][j-1];
+                    else
+                        dp[i][j] = max(dp[i][j-1], dp[i -1][j]);
+                }
+            }
+        }
+        return dp[n][n];
+    }
+};
+
+
 /*
 class Solution:
     # @param A : string
@@ -108,3 +158,85 @@ class Solution:
         
         return L[m][n]
 */
+
+/*
+class Solution {
+public:
+    // Bottom-up approach
+    // TC: O(N*N), SC: O(N*N)
+    int longestPalindromeSubseqTabular(string& s) {
+        if(s.empty())
+            return 0;
+        
+        const int N = s.size();
+        // dp(i, j): length of longest palindromic substring in s[i:j]
+        vector<vector<int> > dp(N, vector<int>(N, 0));
+        
+        // all single chars are palindromic
+        for(int i = 0; i < N; i++)
+            dp[i][i] = 1;
+        
+        for(int l = 1; l < N; l++) {
+            // starting index of window
+            for(int i = 0; i < N - l; i++) {
+                // ending index of window
+                int j = i + l;
+                
+                // if there are only two chars
+                if((j - i + 1) == 2) {
+                    // if the chars are same, then that contributes 2, otherwise since
+                    // individually they are palindromic so max length 1
+                    dp[i][j] = 1 + (s[i] == s[j]);
+                }
+                else {
+                    // for s[i:j]:
+                    // longest palindrome length in s[i+1 : j-1] and +1 if s[i] == s[j]
+                    // if s[i] != s[j], longest length palindrome = Max of longest palin in s[i:j-1] and s[i+1:j]
+                    if(s[i] == s[j])
+                        dp[i][j] = dp[i+1][j-1] + 2;
+                    else
+                        dp[i][j] = max(dp[i+1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[0][N-1];
+    }
+    
+    // Using memoization
+    // TC: O(N*N), SC: O(N*N)
+    int longestPalindSubseqRec(vector<vector<int> >& dp, string& s, int i, int j) {
+        if(i > j)
+            return 0;
+        // when one char
+        if(i == j)
+            return dp[i][j] = 1;
+        
+        if(dp[i][j] == -1) {
+            if(s[i] == s[j])
+                dp[i][j] = 2 + longestPalindSubseqRec(dp, s, i+1, j-1);
+            else
+                dp[i][j] = max(longestPalindSubseqRec(dp, s, i+1, j),
+                               longestPalindSubseqRec(dp, s, i, j-1));
+        }
+        return dp[i][j];
+    }
+    
+    // Recursive driver
+    int longestPalindSubseqRecDriver(string& s) {
+        if(s.empty())
+                return 0;
+
+        const int N = s.size();
+        // dp(i, j): length of longest palindromic substring in s[i:j]
+        vector<vector<int> > dp(N, vector<int>(N, -1)); 
+        
+        return longestPalindSubseqRec(dp, s, 0, N-1);
+    }
+    
+    int longestPalindromeSubseq(string s) {
+        return longestPalindSubseqRecDriver(s);
+        //return longestPalindromeSubseqTabular(s);
+    }
+};
+*/
+
