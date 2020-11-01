722bf13252c29afa0e9cf08f12057756a3e64df3
MANISH KUMAR <36466808+manu05X@users.noreply.github.com>
2020-11-01 13:27:53 +0530

    Added via Upload

--- InterviewBit/String/Partition Labels.cpp
+++ InterviewBit/String/Partition Labels.cpp
@@ -64,3 +64,39 @@ public:
         return res;
     }
 };
+
+// Method 2
+/*
+S: "ababcbacadefegdehijhklij"
+		 {a, b, c,  d,  e,  f,  g,  h,  i,  j,  k,  l}
+end_idx: {8, 5, 7, 14, 15, 11, 13, 19, 22, 23, 20, 21, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+result: {9, 7, 8}
+start: 24
+end: 23
+*/
+
+class Solution {
+public:
+    vector<int> partitionLabels(string S) 
+    {
+        vector<int> end_idx(26, 0);
+        for(int i = 0; i < S.length(); ++i)
+        {
+            end_idx[S[i] - 'a'] = i;
+        }
+        
+        vector<int> result;
+        int start = 0, end = 0;
+        for(int i = 0; i < S.length(); ++i)
+        {
+            end = max(end, end_idx[S[i] - 'a']);
+            if(i == end)
+            { 
+                // all the characters of current partition included
+                result.push_back(i - start + 1);
+                start = i + 1;
+            }
+        }
+        return result;
+    }
+};
